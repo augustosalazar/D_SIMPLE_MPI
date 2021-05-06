dd5e7483d19be9fc7529469c2c2cae02fc1eb5e5
augustosalazar <augustosalazar@uninorte.edu.co>
2021-05-06 15:45:13 -0500

    change output

--- target/primeChecker.py
+++ target/primeChecker.py
@@ -3,7 +3,11 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD   # Defines the default communicator
 num_procs = comm.Get_size()  # Stores the number of processes in num_procs.
 rank = comm.Get_rank()  # Stores the rank (pid) of the current process
+
+
+#at the end
 if rank == 0:
-    print("I am root, have to distribute",sys.argv[1], "numbers")
+    print("Number of primer numbers in",sys.argv[1],"is",9999)
+    print("Total time",222)
 else:
-    print("I am rank",rank, "and I am waiting for my first package")
+    print("I am rank",rank, "I got",888,"packages")
